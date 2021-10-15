6e21c186ce478260de6137560f5639b4a794c1d8
DESKTOP-L4FTB5J\Win 10 <nnbduy239@gmail.com>
2021-10-15 17:29:13 +0700

    commit Cau 5

--- app/src/main/java/com/iuh/a03_18068391_nguyenngocbaoduy_14ctt/MainActivity.java
+++ app/src/main/java/com/iuh/a03_18068391_nguyenngocbaoduy_14ctt/MainActivity.java
@@ -2,7 +2,9 @@ package com.iuh.a03_18068391_nguyenngocbaoduy_14ctt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,5 +22,40 @@ public class MainActivity extends AppCompatActivity {
         btnSub = findViewById(R.id.btnAdd);
         btnMul = findViewById(R.id.btnAdd);
         btnDiv = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNumber();
+                int num1 =Integer.parseInt(editSo1.getText()+"");
+                int num2 =Integer.parseInt(editSo2.getText()+"");
+            }
+        });
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNumber();
+            }
+        });
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNumber();
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getNumber();
+            }
+        });
+    }
+
+    private void getNumber() {
+        Intent intent = new Intent(this,MyService.class);
+        intent.putExtra("number1",editSo1.getText());
+        intent.putExtra("number2",editSo2.getText());
+        startService(intent);
+
     }
 }
--- app/src/main/java/com/iuh/a03_18068391_nguyenngocbaoduy_14ctt/MyService.java
+++ app/src/main/java/com/iuh/a03_18068391_nguyenngocbaoduy_14ctt/MyService.java
@@ -1,10 +1,16 @@
 package com.iuh.a03_18068391_nguyenngocbaoduy_14ctt;
 
+import static com.iuh.a03_18068391_nguyenngocbaoduy_14ctt.AppNotification.CHANNEL_ID;
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.RemoteViews;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     public static final  int ACTION_SUB=1;
@@ -27,11 +33,25 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String number1 = intent.getStringExtra("number1");
+        String number2 = intent.getStringExtra("number2");
         sendNotification();
         return START_NOT_STICKY;
     }
 
     private void sendNotification() {
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.custom_noficationlayout);
+//        remoteViews.setTextViewText(R.id.tvTitle,number.getTitle());
+
+        Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setContentTitle("Ap tinh toan")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1,notification);
+
     }
 
     @Override
